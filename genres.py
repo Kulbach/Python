6ca0c3f6b2f6179746ff6c56e48137e654c91653
Kulbach <polina.alexia@gmail.com>
2019-12-16 18:15:50 +0200

    Deleted unneccessary file

--- genres.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from models import Genre, GenreSchema
-
-
-def read_all():
-    genres = Genre.query.all()
-    genres_schema = GenreSchema(many=True)
-    data = genres_schema.dump(genres).data
-    return data
