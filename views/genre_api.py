2a05b7945253b6baef2f687c51cfadad5944face
Kulbach <polina.alexia@gmail.com>
2020-01-21 21:37:56 +0200

    Added DELETE Genre and tunned methods

--- views/genre_api.py
+++ views/genre_api.py
@@ -5,15 +5,23 @@ from flask import Response
 from schemas.genre_schema import GenreSchema
 from flask import request, abort
 from marshmallow.exceptions import ValidationError
+import json
 
 
 class GenreAPI(MethodView):
     genres_schema = GenreSchema()
 
-    def get(self):
-        genres = Genre.query.all()
-        data = self.genres_schema.dumps(genres, many=True)
-        response = Response(response=data, status=200, mimetype="application/json")
+    def get(self, genre_id):
+        if genre_id is None:
+            genres = Genre.query.all()
+            data = self.genres_schema.dumps(genres, many=True)
+            response = Response(response=data, status=200, mimetype="application/json")
+        else:
+            genre = Genre.query.get(genre_id)
+            if not genre:
+                abort(Response(response='{"message": "Requested genre not found"}', status=404,
+                               mimetype="application/json"))
+            response = Response(response=self.genres_schema.dumps(genre), status=200, mimetype="application/json")
         return response
 
     def post(self):
@@ -21,7 +29,8 @@ class GenreAPI(MethodView):
         try:
             data = self.genres_schema.load(data, many=True)
         except ValidationError as e:
-            abort(400, str(e))
+            abort(Response(response=json.dumps(e.messages), status=400,
+                           mimetype="application/json"))
         db.session.add_all(data)
         db.session.commit()
         response = Response(response=self.genres_schema.dumps(data, many=True), status=200, mimetype="application/json")
@@ -29,8 +38,20 @@ class GenreAPI(MethodView):
 
     def put(self, genre_id):
         genre = Genre.query.get(genre_id)
+        if not genre:
+            abort(Response(response='{"message": "Requested genre not found"}', status=404,
+                           mimetype="application/json"))
         genre.genre = request.json.get('genre', genre.genre)
         db.session.commit()
-        print(genre)
+        response = Response(response=self.genres_schema.dumps(genre), status=200, mimetype="application/json")
+        return response
+
+    def delete(self, genre_id):
+        genre = Genre.query.get(genre_id)
+        if not genre:
+            abort(Response(response='{"message": "Requested genre not found"}', status=404,
+                           mimetype="application/json"))
+        db.session.delete(genre)
+        db.session.commit()
         response = Response(response=self.genres_schema.dumps(genre), status=200, mimetype="application/json")
         return response
